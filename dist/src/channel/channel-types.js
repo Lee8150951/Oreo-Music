57bc81e41b4daf5212a3447a3d878ac80efa0ddc
Yishan Lee <Liys9875@outlook.com>
2023-09-20 23:29:30 +0800

    remove-dist

--- dist/src/channel/channel-types.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.COLOR_CHANNEL = void 0;
-exports.COLOR_CHANNEL = 'color_channel';
-//# sourceMappingURL=channel-types.js.map
