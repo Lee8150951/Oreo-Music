72816222ac0afc987be0698b2e768cd6139f5a01
Yishan Lee <Liys9875@outlook.com>
2023-09-20 23:51:22 +0800

    remove-dist

--- dist/src/event-types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PLAY = exports.DRAWER = void 0;
-// Drawer spread out
-exports.DRAWER = 'drawer_spread';
-// Play song
-exports.PLAY = 'play_song';
-//# sourceMappingURL=event-types.js.map
